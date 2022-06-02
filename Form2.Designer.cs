376de9f1461f2a26cfaa0aca2539abb417c83d2b
LauraKeneberg <Laura.keneberg@gmail.com>
2022-06-03 01:54:14 +0300

    Добавьте файлы проекта.

---
+++ Form2.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SIP
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.lb_SIP = new System.Windows.Forms.Label();
+            this.lb_autoriz = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_login1 = new System.Windows.Forms.Label();
+            this.lb_parol1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bt_vhod = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_SIP
+            // 
+            this.lb_SIP.AutoSize = true;
+            this.lb_SIP.BackColor = System.Drawing.Color.HotPink;
+            this.lb_SIP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lb_SIP.Font = new System.Drawing.Font("Bookman Old Style", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_SIP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lb_SIP.Location = new System.Drawing.Point(12, 22);
+            this.lb_SIP.Name = "lb_SIP";
+            this.lb_SIP.Size = new System.Drawing.Size(77, 32);
+            this.lb_SIP.TabIndex = 1;
+            this.lb_SIP.Text = "СИП";
+            // 
+            // lb_autoriz
+            // 
+            this.lb_autoriz.AutoSize = true;
+            this.lb_autoriz.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_autoriz.Font = new System.Drawing.Font("Bookman Old Style", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_autoriz.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_autoriz.Location = new System.Drawing.Point(891, 292);
+            this.lb_autoriz.Name = "lb_autoriz";
+            this.lb_autoriz.Size = new System.Drawing.Size(229, 39);
+            this.lb_autoriz.TabIndex = 2;
+            this.lb_autoriz.Text = "Авторизация";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox1.Location = new System.Drawing.Point(901, 435);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 36);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lb_login1
+            // 
+            this.lb_login1.AutoSize = true;
+            this.lb_login1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_login1.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_login1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_login1.Location = new System.Drawing.Point(902, 414);
+            this.lb_login1.Name = "lb_login1";
+            this.lb_login1.Size = new System.Drawing.Size(54, 19);
+            this.lb_login1.TabIndex = 4;
+            this.lb_login1.Text = "Логин";
+            // 
+            // lb_parol1
+            // 
+            this.lb_parol1.AutoSize = true;
+            this.lb_parol1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_parol1.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_parol1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_parol1.Location = new System.Drawing.Point(903, 504);
+            this.lb_parol1.Name = "lb_parol1";
+            this.lb_parol1.Size = new System.Drawing.Size(62, 19);
+            this.lb_parol1.TabIndex = 5;
+            this.lb_parol1.Text = "Пароль";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox2.Location = new System.Drawing.Point(901, 525);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 36);
+            this.textBox2.TabIndex = 6;
+            // 
+            // bt_vhod
+            // 
+            this.bt_vhod.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_vhod.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.bt_vhod.Location = new System.Drawing.Point(952, 600);
+            this.bt_vhod.Name = "bt_vhod";
+            this.bt_vhod.Size = new System.Drawing.Size(114, 37);
+            this.bt_vhod.TabIndex = 7;
+            this.bt_vhod.Text = "Вход";
+            this.bt_vhod.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.bt_vhod);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lb_parol1);
+            this.Controls.Add(this.lb_login1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_autoriz);
+            this.Controls.Add(this.lb_SIP);
+            this.Name = "Form2";
+            this.Text = "SIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_SIP;
+        private System.Windows.Forms.Label lb_autoriz;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_login1;
+        private System.Windows.Forms.Label lb_parol1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bt_vhod;
+    }
+}
---
+++ Form3.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SIP
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.bt_vhod = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lb_parol1 = new System.Windows.Forms.Label();
+            this.lb_login1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_autoriz = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_famil = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lb_otch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_vhod
+            // 
+            this.bt_vhod.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bt_vhod.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.bt_vhod.Location = new System.Drawing.Point(840, 602);
+            this.bt_vhod.Name = "bt_vhod";
+            this.bt_vhod.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.bt_vhod.Size = new System.Drawing.Size(175, 37);
+            this.bt_vhod.TabIndex = 13;
+            this.bt_vhod.Text = "Регистрация";
+            this.bt_vhod.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox2.Location = new System.Drawing.Point(735, 424);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 36);
+            this.textBox2.TabIndex = 12;
+            // 
+            // lb_parol1
+            // 
+            this.lb_parol1.AutoSize = true;
+            this.lb_parol1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_parol1.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_parol1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_parol1.Location = new System.Drawing.Point(737, 404);
+            this.lb_parol1.Name = "lb_parol1";
+            this.lb_parol1.Size = new System.Drawing.Size(62, 19);
+            this.lb_parol1.TabIndex = 11;
+            this.lb_parol1.Text = "Пароль";
+            // 
+            // lb_login1
+            // 
+            this.lb_login1.AutoSize = true;
+            this.lb_login1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_login1.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_login1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_login1.Location = new System.Drawing.Point(736, 314);
+            this.lb_login1.Name = "lb_login1";
+            this.lb_login1.Size = new System.Drawing.Size(54, 19);
+            this.lb_login1.TabIndex = 10;
+            this.lb_login1.Text = "Логин";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox1.Location = new System.Drawing.Point(735, 335);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 36);
+            this.textBox1.TabIndex = 9;
+            // 
+            // lb_autoriz
+            // 
+            this.lb_autoriz.AutoSize = true;
+            this.lb_autoriz.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_autoriz.Font = new System.Drawing.Font("Bookman Old Style", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_autoriz.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_autoriz.Location = new System.Drawing.Point(808, 223);
+            this.lb_autoriz.Name = "lb_autoriz";
+            this.lb_autoriz.Size = new System.Drawing.Size(223, 39);
+            this.lb_autoriz.TabIndex = 8;
+            this.lb_autoriz.Text = "Регистрация";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox3.Location = new System.Drawing.Point(1003, 424);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(216, 36);
+            this.textBox3.TabIndex = 17;
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_name.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_name.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_name.Location = new System.Drawing.Point(1005, 404);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(41, 19);
+            this.lb_name.TabIndex = 16;
+            this.lb_name.Text = "Имя";
+            // 
+            // lb_famil
+            // 
+            this.lb_famil.AutoSize = true;
+            this.lb_famil.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_famil.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_famil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_famil.Location = new System.Drawing.Point(1004, 314);
+            this.lb_famil.Name = "lb_famil";
+            this.lb_famil.Size = new System.Drawing.Size(78, 19);
+            this.lb_famil.TabIndex = 15;
+            this.lb_famil.Text = "Фамилия";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox4.Location = new System.Drawing.Point(1003, 335);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(216, 36);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Bookman Old Style", 18F);
+            this.textBox5.Location = new System.Drawing.Point(1003, 499);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(216, 36);
+            this.textBox5.TabIndex = 19;
+            // 
+            // lb_otch
+            // 
+            this.lb_otch.AutoSize = true;
+            this.lb_otch.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lb_otch.Font = new System.Drawing.Font("Bookman Old Style", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_otch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_otch.Location = new System.Drawing.Point(1005, 479);
+            this.lb_otch.Name = "lb_otch";
+            this.lb_otch.Size = new System.Drawing.Size(80, 19);
+            this.lb_otch.TabIndex = 18;
+            this.lb_otch.Text = "Отчество";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lb_otch);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lb_name);
+            this.Controls.Add(this.lb_famil);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.bt_vhod);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lb_parol1);
+            this.Controls.Add(this.lb_login1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_autoriz);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_vhod;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lb_parol1;
+        private System.Windows.Forms.Label lb_login1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_autoriz;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_famil;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lb_otch;
+    }
+}
